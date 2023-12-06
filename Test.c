1094ff51b2b6b52439707ba8cd40c6be56daa5f7
NazmulAlamShawon <shawonkst15@gmail.com>
2023-12-06 09:37:20 +0600

    add git

--- Test.c
+++ Test.c
@@ -1,9 +1,34 @@
 #include<stdio.h>
 
  int main (){
-    int age;
-    printf("enter age :");
-    scanf("%d",&age);
-    age >= 18? printf("adult \n") : printf("not adult \n");
+    int day; // 1-mon ;
+    printf("enter day(1-7) :");
+    scanf("%d",&day);
+     switch (day)
+     {
+     case 1: printf("monday \n");
+        /* code */
+        break;
+        case 2: printf("tuesday \n");
+        /* code */
+        break;
+        case 3: printf("wednesday \n");
+        /* code */
+        break;
+        case 4: printf("thursday \n");        /* code */
+        break;
+        case 5: printf("friday \n"); /* code */
+        /* code */
+        break;
+        case 6: printf("saturday \n"); /* code */
+        /* code */
+        break;
+        
+        case 7: printf("sunday \n"); /* code */
+        /* code */
+        break;
+     default:("not valid date");
+        
+     }
     return 0;
  }
